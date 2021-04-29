2184d45316f687f5f2991db45b6a1dbae6417efa
DorianI <>
2021-04-29 16:31:29 +0000

    small changes to js file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.getElementById('button').addEventListener('click',ChangeStuff)
+document.getElementById('button').addEventListener('click', ChangeStuff)
 
 function ChangeStuff () {
   alert(document.getElementById('txt').value)
-
 }
